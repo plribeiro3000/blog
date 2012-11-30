f30b64d50b2cd07b1c7e9a23f74be8dcfccb79e3
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2012-11-30 11:47:56 -0200

    Added specs for Entry Service

---
+++ spec/services/entry_service_spec.rb
@@ -0,0 +1,67 @@
+require 'spec_helper'
+
+describe EntryService do
+  let(:entry) { Entry.new }
+
+  describe "#index" do
+    before :each do
+      Entry.stub(:all => [entry])
+    end
+
+    it "should return a collection of entries" do
+      EntryService.index.should == [entry]
+    end
+  end
+
+  describe "#find" do
+    before :each do
+      Entry.stub(:find).with("1").and_return(entry)
+    end
+
+    it "should return an specific entry" do
+      EntryService.find("1").should == entry
+    end
+  end
+
+  describe "#new" do
+    before :each do
+      Entry.stub(:new => entry)
+    end
+
+    it "should return a new entry" do
+      EntryService.new.should == entry
+    end
+  end
+
+  describe "#create" do
+    before :each do
+      Entry.stub(:create).with({}).and_return(entry)
+    end
+
+    it "should return a created object" do
+      EntryService.create({}).should == entry
+    end
+  end
+
+  describe "#update" do
+    before :each do
+      Entry.stub(:find).with("1").and_return(entry)
+      entry.stub(:update_attributes => true)
+    end
+
+    it "should return an updated entry" do
+      EntryService.update("1", {}).should == entry
+    end
+  end
+
+  describe "#destroy" do
+    before :each do
+      Entry.stub(:find).with("1").and_return(entry)
+      entry.stub(:destroy => true)
+    end
+
+    it "should return a destroyed object" do
+      EntryService.destroy("1").should == entry
+    end
+  end
+end
