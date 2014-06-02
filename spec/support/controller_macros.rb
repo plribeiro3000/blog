d78ed67ff589c811c2bda8015589c291c1a60daf
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-06-02 17:18:58 -0300

    Fixed Coveralls #wear call. Should be before config/environment load

--- spec/support/controller_macros.rb
+++
@@ -1,8 +0,0 @@
-module ControllerMacros
-  def authenticate_user!
-    @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in User.create( :email => "test@example.com",
-                         :password => "12345678",
-                         :password_confirmation => "12345678" )
-  end
-end
