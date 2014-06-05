7fd10645201dfa99928ea3dcb48456564733caeb
Matheus Mina <mfbmina@gmail.com>
2014-06-05 15:01:56 -0300

    created necessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Blog::Application.routes.draw do
-  devise_for :users
+  #devise_for :users
 
   resources :entries
+
+  resources :users, except: [:edit, :update, :show]
 
   root :to => "root#index"
 end
